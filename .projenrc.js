e6deadfd740f23e396cce4a8fa54ae62d56ed87b
securitywing <syras@securitywing.com>
2021-09-15 12:18:25 +0100

    test

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,31 @@ const project = new JsiiProject({
   author: 'securitywing',
   authorAddress: 'syras@securitywing.com',
   defaultReleaseBranch: 'main',
-  name: 'testmodule',
+  // name: 'cdksecuritywing/testmodule',
+  // repositoryUrl: 'https://github.com/syras/testmodule.git',
+  name: '@cdksecuritywing/testmodule',
   repositoryUrl: 'https://github.com/syras/testmodule.git',
+  jest: false,
+  devDeps: [
+    '@types/semver',
+    '@types/fs-extra',
+    'fs-extra',
+    'glob',
+  ],
+  bundledDeps: [
+    'semver',
+  ],
+  gitignore: [
+    '.idea/',
+  ],
+  deps: ['projen',
+    's3module',
+    '@aws-cdk/core'],
+  peerDeps: ['projen',
+    's3module',
+    '@aws-cdk/core'],
+  npmDistTag: 'latest',
+  npmRegistryUrl: 'https://registry.npmjs.org',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
